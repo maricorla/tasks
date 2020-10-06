3c73609b5750e045d7a3283ac75dd016b217a55e
maricorla <marinagulevichqa@gmail.com>
2020-10-06 15:55:32 +0200

    arrays

---
+++ src/com/marina/tasks/Exercise9.java
@@ -0,0 +1,38 @@
+package com.marina.tasks;
+
+import java.util.Scanner;
+
+public class Exercise9 {
+
+
+        public static void main(String[] args)
+
+        {
+            int i,j,n;
+            System.out.print("Input number of rows : ");
+            Scanner in = new Scanner(System.in);
+            n = in.nextInt();
+
+            for(i=1;i<=n;i++)
+            {
+                for(j=1;j<=i;j++)
+                    System.out.print(i);
+
+                System.out.println("");
+            }
+        }
+
+  /*  int i,j,n,k = 1;
+            System.out.print("Input number of rows : ");
+    Scanner in = new Scanner(System.in);
+    n = in.nextInt();
+
+            for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+            System.out.print(k++);
+
+        System.out.println("");
+    }
+}*/
+    }
