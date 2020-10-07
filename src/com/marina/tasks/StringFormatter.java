0973620a8e0e53439bc62299bf47e8c15761d0ea
maricorla <marinagulevichqa@gmail.com>
2020-10-07 15:34:23 +0200

    how to use %d %s

--- src/com/marina/tasks/StringFormatter.java
+++ src/com/marina/tasks/StringFormatter.java
@@ -1,4 +1,19 @@
 package com.marina.tasks;
 
+import java.util.Scanner;
+
 public class StringFormatter {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = sc.nextLine();
+        System.out.println("Insert number of URA");
+        int counter = sc.nextInt();
+        String result = String.format("Hello %s, URA %d volte",name,counter);
+        System.out.println(result);
+
+        double a = 5.9899889;
+        System.out.println(String.format("Number: %.5f",a));
+    }
 }
